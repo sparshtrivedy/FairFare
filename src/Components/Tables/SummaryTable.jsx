0b8597427ede2adfa93debab048484e9fab4294a
Sparsh Trivedy <sparsh.trivedy2140@gmail.com>
2024-07-11 00:26:55 -0700

    table styling fix

--- src/Components/Tables/SummaryTable.jsx
+++ src/Components/Tables/SummaryTable.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { GoCheckCircle } from "react-icons/go";
-import { ListGroup, Table, Pagination } from "react-bootstrap";
+import { ListGroup, Table, Pagination, Badge } from "react-bootstrap";
 import SettleButton from "../Buttons/SettleButton";
 import ViewButton from "../Buttons/ViewButton";
 
@@ -35,12 +35,10 @@ const SummaryTable = ({ items, id }) => {
 
     return (
         <>
-            <Table striped bordered hover className='p-0 m-0'>
+            <Table striped bordered hover className='p-0 m-0 w-100'>
                 <thead>
                     <tr>
-                        <th>Item name</th>
-                        <th>Event name</th>
-                        <th>Amount</th>
+                        <th>item</th>
                         <th>{id === "you-owe" ? "To" : "From"}</th>
                         <th>Actions</th>
                     </tr>
@@ -60,9 +58,28 @@ const SummaryTable = ({ items, id }) => {
                 ) : (
                     currentItems.map((item, index) => (
                         <tr key={`${id}_${index}`}>
-                            <td>{item.itemName}</td>
-                            <td>{item.eventName}</td>
-                            <td>{item.amount}</td>
+                            <td>
+                                <div className="d-flex justify-content-between">
+                                    <div className="d-flex flex-column" style={{ paddingRight: '10px' }}>
+                                        <span className="fw-normal text-nowrap">
+                                        {item.itemName}
+                                        </span>
+                                        <p className="text-muted fw-light text-nowrap">{item.eventName}</p>
+                                    </div>
+                                    <div className="d-flex flex-column align-items-end" style={{ paddingLeft: '10px' }}>
+                                        <span className="fw-bold text-muted">$ {item.amount}</span>
+                                        {item.eventName === "N/A"  ? (
+                                            <Badge bg="warning" text="dark">
+                                                Standalone
+                                            </Badge>
+                                        ) : (
+                                            <Badge bg="info" text="dark">
+                                                Associated
+                                            </Badge>
+                                        )}
+                                    </div>
+                                </div>
+                            </td>
                             <td className="p-0">
                                 <ListGroup variant="flush">
                                 {item.members.map((member, index) => (
@@ -70,7 +87,7 @@ const SummaryTable = ({ items, id }) => {
                                 ))}
                                 </ListGroup>
                             </td>
-                            <td>
+                            <td className="text-nowrap display-inline-block" style={{ width: '225px' }}>
                                 <ViewButton id={id} path={item.eventId ? `/#/view-event/${item.eventId}` : `/#/view-item/${item.id}`} />
                                 <SettleButton item={item} />
                             </td>
@@ -80,7 +97,7 @@ const SummaryTable = ({ items, id }) => {
                 </tbody>
             </Table>
             {items.length > 0 &&
-                <Pagination className="mt-3">
+                <Pagination className="mt-3 mb-0">
                     <Pagination.Prev onClick={handlePrev} disabled={currentPage === 1} />
                     {pageNumbers.map(number => (
                         <Pagination.Item key={number} active={number === currentPage} onClick={() => handleClick(number)}>
